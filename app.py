113eb02a92d578cf88a9358149f80a064df6ca28
Richa Roy <richaroyghy@gmail.com>
2026-04-23 18:25:48 +0000

    Update Flask app host to '0.0.0.0' and enhance API URL handling in index.html

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ if __name__ == '__main__':
         
         # Run the Flask app
         app.run(
-            host='localhost',
+            host='0.0.0.0',
             port=5000,
             debug=False,
             use_reloader=False
